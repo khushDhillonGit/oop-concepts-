660f9ab7ce285d4fbb4f0cfe1d44794d2eda3099
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2023-06-11 16:40:19 -0400

    added logic o move from room to rooom

--- oop-concepts/Src/Map/House.Move.cs
+++ oop-concepts/Src/Map/House.Move.cs
@@ -2,11 +2,16 @@
 
 public partial class House {
 
-    public void GoToRoom(int index) { 
-    
+    public Room CurrentRoom { get; set; }
+    public void GoToRoom(int index) 
+    {
+        if (CurrentRoom != null) { 
+            CurrentRoom.Visited = true;
+        }
+        CurrentRoom = Rooms[index];
     }
 
     public void GoToStartingRoom() { 
-    
+        GoToRoom(_rnd.Next(0,Rooms.Length));
     }
 }
--- oop-concepts/Src/Map/House.cs
+++ oop-concepts/Src/Map/House.cs
@@ -6,6 +6,10 @@ public partial class House
     
     private readonly Random _rnd = new (1234);
 
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public Room[] Rooms { get; set; }   
+
     //only constructer can set the player field, otherwise its a get field
     public House(Player player)
     {
